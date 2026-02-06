3b15eaf090700dc3714bd9f355a556a83ed217e5
siddique313 <siddiqueali531@gmail.com>
2026-02-06 22:22:19 +0500

    wip

--- server.js
+++ server.js
@@ -223,6 +223,6 @@ io.on("connection", (socket) => {
 /* ================= START SERVER ================= */
 
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, "0.0.0.0" || 3001, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
